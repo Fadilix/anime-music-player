98b84e291607335e3d99423bd1e2919822c5d23f
Fadilix <iamfadile2@gmail.com>
2024-04-02 11:31:22 +0000

    first commit

---
+++ src/data/data.ts
@@ -0,0 +1,26 @@
+import snk from "../sounds/snk_sasageyo.mp3";
+import snkBg from "../assets/snk_sasageyo.jpg";
+export const soundtrack = [
+  {
+    name: "Download Shingeki no Kyojin 2 Opening - Shinzou wo Sasageyo!",
+    src: snk,
+    bg: snkBg,
+  },
+  {
+    name: "One Punch Man Opening - THE HERO",
+    src: ""
+
+  },
+
+  {
+    name: "Hunter x Hunter Opening - Departure",
+  },
+
+  {
+    name: "Kimetsu No Yaiba - Opening 1",
+  },
+
+  {
+    name: "Black Clover Opening 3 - Black Rover",
+  },
+];
