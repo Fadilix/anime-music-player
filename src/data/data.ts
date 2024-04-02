9e207021ce22ea57a374938cf2ded86faa70842b
Fadilix <iamfadile2@gmail.com>
2024-04-02 11:37:41 +0000

    setting up the data for the app

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,5 +1,15 @@
+// Setting up the data for the application
 import snk from "../sounds/snk_sasageyo.mp3";
 import snkBg from "../assets/snk_sasageyo.jpg";
+import blackC from "../sounds/black_clover_black_rover.mp3";
+import kny from "../sounds/kny_op1.mp3";
+import opm from "../sounds/opm.mp3";
+import blackCbg from "../assets/bc.jpeg";
+import knybg from "../assets/kny.jpg";
+import opmbg from "../assets/opm.jpg";
+import hxh from "../sounds/hxh_dep.mp3";
+import hxhbg from "../assets/hxh.jpg";
+
 export const soundtrack = [
   {
     name: "Download Shingeki no Kyojin 2 Opening - Shinzou wo Sasageyo!",
@@ -8,19 +18,25 @@ export const soundtrack = [
   },
   {
     name: "One Punch Man Opening - THE HERO",
-    src: ""
-
+    src: opm,
+    bg: opmbg,
   },
 
   {
     name: "Hunter x Hunter Opening - Departure",
+    src: hxh,
+    bg: hxhbg,
   },
 
   {
     name: "Kimetsu No Yaiba - Opening 1",
+    src: kny,
+    bg: knybg,
   },
 
   {
     name: "Black Clover Opening 3 - Black Rover",
+    src: blackC,
+    bg: blackCbg,
   },
 ];
